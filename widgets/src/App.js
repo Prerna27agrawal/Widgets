33960c298b1cac45da44862fe1807aaa3c7c2408
Prerna27agrawal <agrawalprerna1999@gmail.com>
2021-01-04 22:04:15 +0530

    added searrch part

---
+++ widgets/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+const items =[
+    {
+        title:'Wha is React?',
+        content:'React is a front end js framework'
+    },
+    {
+        title:'Wha is Reactfffffff?',
+        content:'React is a front fffffffffffffeeeeend js framework'
+    },
+    {
+        title:' hdsshf?',
+        content:'React ishgdfajhdygsdjhusydga amework'
+    }
+]
+
+export default ()=>{
+    return (
+    <div>
+     <Dropdown />
+    </div>
+        );
+};
